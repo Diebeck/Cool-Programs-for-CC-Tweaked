0b389b3d6f4b30a9bc9deea13f665d81fc1402d4
Diebeck <141666086+Diebeck@users.noreply.github.com>
2024-10-01 22:02:36 +0200

    Add files via upload

---
+++ startup.lua.lua
@@ -0,0 +1,11 @@
+term.clear()
+term.setCursorPos(1,1)
+print("AI STARTUP INICIATED")
+print("This is computer [ "..os.getComputerID().." ]")
+print("Terminate the program for manual startup.")
+print("Please wait...")
+
+rednet.open("top")
+local _, message = rednet.receive("AI")
+print(message[1])
+shell.run(table.unpack(message))
