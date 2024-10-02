77a9703d07e446ff6215d29e00fede03e6eb8264
Diebeck <141666086+Diebeck@users.noreply.github.com>
2024-10-02 21:03:06 +0200

    Update transmitter.lua

--- transmitter.lua
+++ transmitter.lua
@@ -1,5 +1,27 @@
-local message = {...}
-print("Target Computer:")
+peripheral.find("modem", rednet.open)
+rednet.broadcast("","discover")
+
+local function listen()
+  while true do
+    local offer = rednet.receive("offer",0.1)
+    if offer ~= nil then
+      term.write(offer)
+      term.write(" ")
+    end
+  end
+end
+
+local function timeout()
+  sleep(0.1)
+end
+
+print("Available computers:")
+parallel.waitForAny(listen, timeout)
+
+print("")
+print("Target:")
 local target = tonumber(read())
-rednet.open("top")
+print("Message:")
+local message = tostring(read())
+
 rednet.send(target,message,"run")
