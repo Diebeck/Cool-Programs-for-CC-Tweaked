254c623c9106e0a63d958b9244cba569b97dd56a
Diebeck <141666086+Diebeck@users.noreply.github.com>
2024-10-02 21:03:36 +0200

    Update receiver.lua

--- receiver.lua
+++ receiver.lua
@@ -1,3 +1,5 @@
+peripheral.find("modem", rednet.open)
+
 term.clear()
 term.setCursorPos(1,1)
 print("RECEIVING INSTRUCTIONS")
@@ -5,7 +7,36 @@ print("This is computer [ "..os.getComputerID().." ]")
 print("Terminate the program for manual startup.")
 print("Please wait...")
 
-rednet.open("top")
-local _, message = rednet.receive("run")
-print(message[1])
-shell.run(table.unpack(message))
+local command
+
+local function getMessage()
+  local _, message = rednet.receive("run")
+  print("Running \""..message.."\"")
+  command = message
+end
+
+local function offer()
+  while true do
+    local pinger = rednet.receive("discover",1)
+    if pinger then
+      rednet.send(pinger, os.getComputerID(), "offer")
+    end
+  end
+end
+
+local function anim()
+  while true do
+    term.setCursorPos(1,4)
+    term.write("Please wait.  ")
+    sleep(0.5)
+    term.setCursorPos(1,4)
+    term.write("Please wait.. ")
+    sleep(0.5)
+    term.setCursorPos(1,4)
+    term.write("Please wait...")
+    sleep(0.5)
+  end
+end
+
+parallel.waitForAny(getMessage, anim, offer)
+shell.run(command)
