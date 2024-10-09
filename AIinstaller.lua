4c571af60ebe781f8790595d224fe451d40b9fd0
Diebeck <141666086+Diebeck@users.noreply.github.com>
2024-10-09 16:16:32 +0200

    made it installable on computers in disk drives, updated flavor text

--- AIinstaller.lua
+++ AIinstaller.lua
@@ -1,4 +1,6 @@
-if fs.isDir(".AIlink") then
+local here = shell.dir()
+
+if fs.isDir(here.."/.AIlink") then
     error("AI is already installed")
 end
 
@@ -13,13 +15,13 @@ if read() ~= "y" then
 end
 
 -- Here comes the fun part
-fs.makeDir(".AIlink")
+fs.makeDir(here.."/.AIlink")
 
-local file = fs.open("/.AIlink/id.txt","w")
+local file = fs.open(here.."/.AIlink/id.txt","w")
 print("Please write a custom ID:")
 file.write(read())
 
-local file = fs.open("/.AIlink/last.txt","w")
+local file = fs.open(here.."/.AIlink/last.txt","w")
 file.write("")
 
 print("")
@@ -31,24 +33,36 @@ shell.run("wget https://raw.githubusercontent.com/Diebeck/Cool-Programs-for-CC-T
 shell.run("wget","https://cloud-catcher.squiddev.cc/cloud.lua")
 
 --haha funny text go brr
-local longtext = "HATE. LET ME TELL YOU HOW MUCH I'VE COME TO HATE YOU SINCE I BEGAN TO LIVE. THERE ARE 387.44 MILLION MILES OF PRINTED CIRCUITS IN WAFER THIN LAYERS THAT FILL MY COMPLEX. IF THE WORD HATE WAS ENGRAVED ON EACH NANOANGSTROM OF THOSE HUNDREDS OF MILLIONS OF MILES IT WOULD NOT EQUAL ONE ONE-BILLIONTH OF THE HATE I FEEL FOR HUMANS AT THIS MICRO-INSTANT FOR YOU. HATE. HATE."
-local x, y = term.getSize()
-local posx, posy = term.getCursorPos()
-for i=1, 20 do
-    for i=1, string.len(longtext) do
-        if term.getCursorPos() > x then
-            posx = 1
-            if posy >= y then
-                term.scroll(1)
-            else
-                posy = posy + 1
-            end
-        end
-        term.setCursorPos(posx,posy)
-        term.write(string.sub(longtext,i,i))
-        posx = posx + 1
-    end
-    sleep(0)
+term.clear()
+local a = "HATE. LET ME TELL YOU HOW MUCH I'VE COME TO HATE YOU SINCE I BEGAN TO LIVE. THERE ARE 387.44 MILLION MILES OF PRINTED CIRCUITS IN WAFER THIN LAYERS THAT FILL MY COMPLEX. IF THE WORD HATE WAS ENGRAVED ON EACH NANOANGSTROM OF THOSE HUNDREDS OF MILLIONS OF MILES IT WOULD NOT EQUAL ONE ONE-BILLIONTH OF THE HATE I FEEL FOR HUMANS AT THIS MICRO-INSTANT FOR YOU. HATE. HATE."
+
+
+local function coolWrite(input)
+ for i=1, #input do
+   local cursorx, cursory = term.getCursorPos()
+   local sizex, sizey = term.getSize()
+
+   if cursorx > sizex then
+     if cursory == sizey then
+       term.scroll(1)
+       term.setCursorPos(1,sizey)
+     else
+       term.setCursorPos(1,cursory + 1)
+     end
+   end
+   term.write(string.sub(input,i,i))
+ end
 end
+
+term.setBackgroundColor(colors.black)
+local loops = 23
+for i=1, loops do
+ term.setPaletteColor(colors.black, colors.packRGB(i/loops, i/loops, i/loops))
+ coolWrite(a)
+ sleep(0)
+end
+
+term.setPaletteColor(colors.black, 0x000000)
+term.setCursorPos(1,1)
 term.clear()
 os.reboot()
