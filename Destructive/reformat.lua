8d84d9f5c11b86cbc76c188a1f2d5c16772e213e
Diebeck <romeoreyalonso@gmail.com>
2025-01-18 20:25:15 +0100

    Create reformat.lua

---
+++ Destructive/reformat.lua
@@ -0,0 +1,15 @@
+-- RUNNING THIS CODE WILL RESET ALL OF THE COMPUTER'S FILES
+-- YOU HAVE BEEN WARNED
+
+print("You are about to delete all files on this computer.")
+print("Are you sure you want to proceed? (y/n)")
+if read() ~= "y" then return end 
+
+pcall(function()
+    local all = fs.list("/")
+    for i=1, #all do
+        if fs.exists(all[i]) then
+            fs.delete(all[i])
+        else
+    end
+)
