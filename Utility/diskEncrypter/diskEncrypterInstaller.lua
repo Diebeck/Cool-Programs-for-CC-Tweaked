afd1f54473d5c4fc11b9a9b584927a5db9e96cd2
Diebeck <romeoreyalonso@gmail.com>
2025-01-20 23:34:06 +0100

    Update diskEncrypterInstaller.lua

--- Utility/diskEncrypter/diskEncrypterInstaller.lua
+++ Utility/diskEncrypter/diskEncrypterInstaller.lua
@@ -1,2 +1,2 @@
-shell.run("wget https://raw.githubusercontent.com/Diebeck/CCTweaked-Program-Stash/refs/heads/main/Utility/diskEncrypter/encrypt")
+shell.run("wget https://raw.githubusercontent.com/Diebeck/CCTweaked-Program-Stash/refs/heads/main/Utility/diskEncrypter/encrypt.lua")
 shell.run("wget https://raw.githubusercontent.com/Diebeck/CCTweaked-Program-Stash/refs/heads/main/Utility/diskEncrypter/startup.lua")
